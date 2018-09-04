e36da8cacfc098caaa9f6c9f2a4f32c1f63fdf6b
James Foster <github@jgfoster.net>
2018-09-03 22:08:28 -0700

    We seem to have a working example of using Topaz as a Jupyter kernel!

---
+++ GS-Jupyter-3.4.2/build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ ! -d gemstone/data ]; then
+	mkdir gemstone/data
+fi
+if [ ! -d gemstone/locks ]; then
+	mkdir gemstone/locks
+fi
+if [ ! -d gemstone/log ]; then
+	mkdir gemstone/log
+fi
+if [ ! -f GemStone.zip ]; then
+	curl https://downloads.gemtalksystems.com/pub/GemStone64/3.4.2/GemStone64Bit3.4.2-x86_64.Linux.zip > GemStone.zip
+fi
+if [ ! -f gemstone/product/version.txt ]; then
+	chmod -R 777 gemstone/product
+	rm -rf gemstone/product
+	unzip GemStone.zip
+	mv GemStone*.Linux gemstone/product
+fi
+if [ ! -f gemstone/data/extent0.dbf ]; then
+	cp gemstone/product/bin/extent0.dbf gemstone/data
+	chmod +w gemstone/data/extent0.dbf
+fi
+if [ ! -d gemstone/topaz_kernel ]; then
+	git clone https://github.com/jgfoster/topaz_kernel.git gemstone/topaz_kernel
+fi
